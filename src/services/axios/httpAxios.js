ca95af23244c2e913433858c5156724ec742651a
IVSON VITAL <ivsonv@gmail.com>
2021-11-10 10:46:00 -0300

    axios RETRY

--- src/services/axios/httpAxios.js
+++ src/services/axios/httpAxios.js
@@ -1,10 +1,25 @@
 import axios from 'axios'
+import axiosRetry from 'axios-retry'
 
 const middleware = axios.create({
     baseURL: ''
 })
+
 middleware.interceptors.request.use((config) => {
     config.headers.Authorization = `Bearer ${localStorage.getItem('accessToken')}`
     return config
 })
+
+const retryDelay = (retryNumber = 0) => {
+    const seconds = Math.pow(2, retryNumber) * 1000;
+    const randomMs = 1000 * Math.random();
+    return seconds + randomMs;
+};
+
+axiosRetry(axios, {
+    retries: 2,
+    retryDelay,
+    // retry on Network Error & 5xx responses
+    retryCondition: axiosRetry.isRetryableError,
+});
 export default middleware
